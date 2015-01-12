534f9369c4e796eb2617565500a8cb02c31dd94f
longzai1988 <longzai88@gmail.com>
2015-01-12 20:42:18 +0800

    Version 1.4.8.1

--- html/ui/js/spn.js
+++ html/ui/js/spn.js
@@ -1,5 +1,5 @@
 var SPN = (function (SPN, $, undefined) {
-    SPN.version = "1.4.7.2";
+    SPN.version = "1.4.8.1";
     SPN.isCoinConnect = false;
     var coinAddress = "";
     var isCoinAddressAvailable = true;
