5407bb87e6bc21c9d3b5a51dc744ccca3bafc8b6
Brandon Collins <brandoncollins@gmail.com>
2015-02-22 13:59:55 -0500

    flow fixes

--- html/ui/js/spn.nxtvault.js
+++ html/ui/js/spn.nxtvault.js
@@ -5,40 +5,18 @@ var SPN = (function(SPN, $){
 
     var isDebug = true;
 
+    var accountLinked = null;
+
     $("#spn_nxtvault").click(function(){
         var pubKey = NRS.publicKey;
 
-        $.ajax({
-            url: URL,
-            dataType: 'json',
-            contentType: 'application/json; charset=UTF-8',
-            type: 'POST',
-            timeout: 30000,
-            crossDomain: true,
-            data: JSON.stringify({cmd: "getAccountByLinkedPubKey", pubKey: NRS.publicKey}),
-            success: function (data) {
-                $("#spn_nxtvault_link, #spn_nxtvault_fund_amount").removeAttr('disabled', 'disabled');
-
-                if (data.errorCode == 0){
-                    $("#linkedMessage").text("Account is locked and protected by: " + NRS.convertNumericToRSAccountFormat(NRS.getAccountIdFromPublicKey(data.data.masterPubKey)));
-                    $("#linkedMessage").show();
-                    $("#spn_nxtvault_link, #spn_nxtvault_fund_amount").attr('disabled', 'disabled');
-                    $("#spn_nxtvault_loading").hide();
-                }
-            },
-            error: function(data){
-                $("#linkedMessage").text("There was an error. Please check your verification code and try again").show();
-                $("#linkedMessage").show();
-                $("#linkedMessage").show();
-                $("#spn_nxtvault_link, #spn_nxtvault_fund_amount").attr('disabled', 'disabled');
-                $("#spn_nxtvault_loading").hide();
-            }
-        });
+        $("#linkedMessage").hide();
+        checkLinkedAccount();
     });
 
 
     $("#spn_nxtvault_link, #spn_nxtvault_fund_amount").on("input", function(){
-        if ($("#spn_nxtvault_link").val() != "" && $("#spn_nxtvault_fund_amount") != ""){
+        if ((accountLinked || $("#spn_nxtvault_link").val() != "") && $("#spn_nxtvault_fund_amount") != ""){
             $("#spn_nxtvault_activate").removeClass("disabled");
         }
         else{
@@ -71,38 +49,60 @@ var SPN = (function(SPN, $){
 
 
         //Make the request to link the account.
-        $.ajax({
-            url: URL,
-            dataType: 'json',
-            contentType: 'application/json; charset=UTF-8',
-            type: 'POST',
-            timeout: 30000,
-            crossDomain: true,
-            data: JSON.stringify({cmd: "verify", code: linkCode, accountId: nxtAccountId}),
-            success: function (data) {
-                if (data.errorCode === 0){
+        if (!accountLinked){
+            $.ajax({
+                url: URL,
+                dataType: 'json',
+                contentType: 'application/json; charset=UTF-8',
+                type: 'POST',
+                timeout: 30000,
+                crossDomain: true,
+                data: JSON.stringify({cmd: "verify", code: linkCode, accountId: nxtAccountId}),
+                success: function (data) {
+                    if (data.errorCode === 0){
+                        //Send the required initial funding money to the master account
+                        NRS.sendRequest("sendMoney", {"secretPhrase": secret, feeNQT: "100000000", deadline: "1440", recipient: data.data.masterRs, recipientPublicKey: data.data.masterPubKey, amountNXT: fundAmount}, function (response, input) {
+                            if (!response.errorCode || response.errorCode == 0) {
+                                $("#linkedMessage").show();
+                                $("#fund_message").text("Fund Successful!");
+                                $('#fund_message').show();
+                                checkLinkedAccount();
+                            }
+                            else {
+                                $('#fund_message').show();
+                                $("#fund_message").text(response.errorDescription);
+                            }
 
-                    //Send the required initial funding money to the master account
-                    NRS.sendRequest("sendMoney", {"secretPhrase":secret,feeNQT:"100000000",deadline:"1440",recipient:data.data.masterRs,publicKey:data.data.masterPubKey,amountNXT:fundAmount}, function(response, input) {
-                        if (!response.errorCode == 1){
-                            $("#linkedMessage").show();
-                        }
-                        else{
-                            $("#linkedMessage").text(response.errorDescription);
-                            $("#linkedMessage").show();
-                        }
+                            $("#spn_nxtvault_loading").hide();
+                        }, false);
+                    }
+                    else{
+                        showError(data.errorText);
+                    }
+                },
+                error: function(data){
+                    showError();
+                }
+            });
+        }
+        else{
+            //fund an already initialized account
+            NRS.sendRequest("sendMoney", {"secretPhrase": secret, feeNQT: "100000000", deadline: "1440", recipient: accountLinked, amountNXT: fundAmount}, function (response, input) {
+                if (!response.errorCode || response.errorCode == 0) {
+                    $("#linkedMessage").show();
+                    $("#fund_message").text("Fund Successful!");
+                    $('#fund_message').show();
+                    checkLinkedAccount();
+                }
+                else {
+                    $('#fund_message').show();
+                    $("#fund_message").text(response.errorDescription);
+                }
 
-                        $("#spn_nxtvault_loading").hide();
-                    },false);
-                }
-                else{
-                    showError(data.errorText);
-                }
-            },
-            error: function(data){
-                showError();
-            }
-        });
+                $("#spn_nxtvault_loading").hide();
+            }, false);
+        }
+
 
         function showError(text){
             if (!text){
@@ -116,6 +116,41 @@ var SPN = (function(SPN, $){
             $("#spn_nxtvault_loading").hide();
         }
     });
+
+    function checkLinkedAccount(){
+        $.ajax({
+            url: URL,
+            dataType: 'json',
+            contentType: 'application/json; charset=UTF-8',
+            type: 'POST',
+            timeout: 30000,
+            crossDomain: true,
+            data: JSON.stringify({cmd: "getAccountByLinkedPubKey", pubKey: NRS.publicKey}),
+            success: function (data) {
+                $("#spn_nxtvault_link, #spn_nxtvault_fund_amount").removeAttr('disabled', 'disabled');
+
+                if (data.errorCode == 0){
+                    accountLinked = NRS.convertNumericToRSAccountFormat(NRS.getAccountIdFromPublicKey(data.data.masterPubKey));
+
+                    $("#linkedMessage").text("Account is locked and protected by: " + accountLinked);
+                    $("#linkedMessage").show();
+                    $("#spn_nxtvault_link").attr('disabled', 'disabled');
+                    $("#spn_nxtvault_loading").hide();
+                    $("#activate_message").hide();
+                    $("#spn_nxtvault_activate").text("Fund");
+                    $(".account_link_code").hide();
+                }
+            },
+            error: function(data){
+                accountLinked = null;
+
+                $("#linkedMessage").text("There was an error. Please check your verification code and try again").show();
+                $("#linkedMessage").show();
+                $("#spn_nxtvault_link, #spn_nxtvault_fund_amount").attr('disabled', 'disabled');
+                $("#spn_nxtvault_loading").hide();
+            }
+        });
+    }
 
     return SPN;
 }(SPN || {}, jQuery));
